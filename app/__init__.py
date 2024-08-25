d2c365953e12a8ce2345e86891ea2c474239ef77
Serhiy Hull <canyonserg34@gmail.com>
2024-08-25 20:27:19 +0300

    В __init__.py вніс зміни та config.py. Створив файл models де знаходиться модель таблиці User в базі даних.

--- app/__init__.py
+++ app/__init__.py
@@ -6,5 +6,5 @@ from flask_migrate import Migrate
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
-migrate = SQLAlchemy(app, db)
+migrate = Migrate(app, db)
 from app import routes, models
--- config.py
+++ config.py
@@ -4,4 +4,4 @@ basedir = os.path.abspath(os.path.abspath(__file__))
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-guess'
-    SQLACLHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
